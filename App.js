66b0aba6c203fb946d30e4f52304e4de6ea09ff3
Jyoti <jyoti.pund@intelliswift.com>
2025-03-20 13:09:39 +0530

    episode 03

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = [
-  React.createElement("h1", { id: "heading" }, "This is h1 tag"),
-  React.createElement("h1", { id: "heading" }, "This is h2 tag"),
-];
+// const heading = React.createElement("h1", { id: "heading" }, "This is h1 tag");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+//React Element
+const Title = () => <h1 id="heading">This is a react fun</h1>;
+const elem = <span>Good Morning</span>;
+const myname = <h1> {elem} Hey I am jyoti</h1>;
+
+//React Functional Component
+//Component Compositions
+const Expresion = 10 + 12;
+const Heading1 = () => {
+  return (
+    <div>
+      {myname}
+      {Expresion === 22 ? "you won" : "you lost"}
+      {/* <Title /> */}
+      {Title()} {/*we can call function like this as well */}
+      <h1 id="heading1">This a react component</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+
+root.render(<Heading1 />); //render functional component
